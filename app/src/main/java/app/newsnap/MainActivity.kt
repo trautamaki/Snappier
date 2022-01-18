1570cf57988644a2e0f96e8048910c31896322fc
Spirtix <michal.prazak1@gmail.com>
2022-01-18 16:27:14 +0100

    Use ImageCapturer object

--- app/src/main/java/app/newsnap/MainActivity.kt
+++ app/src/main/java/app/newsnap/MainActivity.kt
@@ -2,31 +2,26 @@ package app.newsnap
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import app.newsnap.capturer.ImageCapturer
 import app.newsnap.ui.OptionsBar.IOptionsBar
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
-import java.text.SimpleDateFormat
-import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity(), IOptionsBar {
-    private var imageCapture: ImageCapture? = null
     private var lensFacing: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
     private lateinit var viewFinder: ViewFinder
+    private lateinit var imageCapturer: ImageCapturer
     private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -73,41 +68,7 @@ class MainActivity : AppCompatActivity(), IOptionsBar {
 
     private fun takePhoto() {
         viewFinder.unFadeControls()
-
-        // Get a stable reference of the modifiable image capture use case
-        val imageCapture = imageCapture ?: return
-
-        val mediaDir = externalMediaDirs.firstOrNull()?.let {
-            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
-        }
-        val outputDirectory = if (mediaDir != null && mediaDir.exists())
-            mediaDir else filesDir
-
-        // Create time-stamped output file to hold the image
-        val photoFile = File(
-                outputDirectory,
-                SimpleDateFormat(FILENAME_FORMAT, Locale.US
-                ).format(System.currentTimeMillis()) + ".jpg")
-
-        // Create output options object which contains file + metadata
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
-
-        // Set up image capture listener, which is triggered after photo has
-        // been taken
-        imageCapture.takePicture(
-                outputOptions, ContextCompat.getMainExecutor(this),
-                object : ImageCapture.OnImageSavedCallback {
-                    override fun onError(exc: ImageCaptureException) {
-                        Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
-                    }
-
-                    override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                        val savedUri = Uri.fromFile(photoFile)
-                        val msg = "Photo capture succeeded: $savedUri"
-                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                        Log.d(TAG, msg)
-                    }
-                })
+        imageCapturer.takePhoto()
     }
 
     private fun swapCamera() {
@@ -128,15 +89,14 @@ class MainActivity : AppCompatActivity(), IOptionsBar {
         cameraProviderFuture.addListener({
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
-            imageCapture = ImageCapture.Builder()
-                    .build()
+            imageCapturer = ImageCapturer(this)
 
             try {
                 // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
 
                 // Build viewfinder
-                viewFinder.build(cameraProvider, lensFacing, imageCapture!!);
+                viewFinder.build(cameraProvider, lensFacing, imageCapturer.imageCapture);
 
             } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
@@ -158,7 +118,7 @@ class MainActivity : AppCompatActivity(), IOptionsBar {
     }
 
     override fun onFlashToggled(flashMode: Int) {
-        imageCapture?.flashMode = flashMode
+        imageCapturer.imageCapture.flashMode = flashMode
     }
 
     override fun onOptionsBarClick() {
